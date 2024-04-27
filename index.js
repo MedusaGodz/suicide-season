27fe702ff7270c442e608072d1b7fa797e821631
Medusa <168293107+MedusaGodz@users.noreply.github.com>
2024-04-28 00:41:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    express:true
+}))
+mongoose.connect('mongodb://localhost:27017/qrcodedb')
+var db=mongoose.connection
+db.on('error',()=> console.log("Error in Connecting to Database"))
+db.once('open', () => console.log("Connected to Database"))
+
+app.post("/index.html",(req,res) => {
+    var fname=req.body.fname
+    var email=req.body.email
+    var cnumber=req.body.cnumber
+    var address=req.body.address
+
+    var data={
+        "fname": fname,
+        "email": email,
+        "cnumber": cnumber,
+        "address": address
+  
+    }
+    db.collection('users').insertOne(data,(err,collection) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Successfully Submit!")
+    })
+    return res.redirect('qrcode.html')
+})
+app.get("/",(req,res) => {
+    res.set({
+        "Allow-access-Allow-Origin":'*'
+    }) 
+    return res.redirect('index.html')
+}).listen(3000);
+
+console.log("Listening on Port 3000")
